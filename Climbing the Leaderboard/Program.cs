f0b819de36d1494f3c3463f81dd4cf0b9cef2f89
Melike Aydın <melike.ay45@gmail.com>
2024-07-04 16:41:36 +0300

    [Feat]: Added Climbing the Leaderboard exam.

---
+++ Climbing the Leaderboard/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    /*
+     * Complete the 'climbingLeaderboard' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY ranked
+     *  2. INTEGER_ARRAY player
+     */
+    public static List<int> climbingLeaderboard(List<int> ranked, List<int> player)
+    {
+        List<int> distinctRanks = ranked.Distinct().ToList();
+        distinctRanks.Reverse();
+
+        List<int> result = new List<int>();
+        int i = 0;
+
+        foreach (var score in player)
+        {
+            while (i < distinctRanks.Count && score >= distinctRanks[i])
+            {
+                i++;
+            }
+            result.Add(distinctRanks.Count - i + 1);
+        }
+
+        return result;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int rankedCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> ranked = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(rankedTemp => Convert.ToInt32(rankedTemp)).ToList();
+
+        int playerCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> player = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(playerTemp => Convert.ToInt32(playerTemp)).ToList();
+
+        List<int> result = Result.climbingLeaderboard(ranked, player);
+
+        textWriter.WriteLine(String.Join("\n", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
